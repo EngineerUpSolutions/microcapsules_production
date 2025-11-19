0df5254e40ecf9d703956622ae0113923c4f6d25
EngineerUpSolutions <software.expertise.going@gmail.com>
2025-11-19 15:30:11 -0500

    working on the fronend

---
+++ front-end/next.config.ts
@@ -0,0 +1,17 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   basePath: "/microcapsulas",
+//   assetPrefix: "/microcapsulas/",
+// };
+
+// export default nextConfig;
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  basePath: "/microcapsulas",
+  assetPrefix: "/microcapsulas",
+};
+
+export default nextConfig;
+
