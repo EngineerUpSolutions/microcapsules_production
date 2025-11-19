508508500ef52065b77875f735a1f1e403a6813a
EngineerUpSolutions <software.expertise.going@gmail.com>
2025-11-19 10:55:55 -0500

    plugin v1.2 new tab working

--- plugin/microcapsulas/index_origin.php
+++ plugin/microcapsulas/index_origin.php
@@ -2,13 +2,17 @@
 require_once(__DIR__ . '/../../config.php');
 require_login();
 
-$externalurl = "http://localhost:3000";
+$externalurl = "http://localhost:3000"; 
+// In production change to: https://domain/microcapsulas
 
 echo $OUTPUT->header();
 
 echo "
 <script>
+    // Open frontend in NEW TAB
     window.open('$externalurl', '_blank');
+
+    // Keep Moodle tab intact (go back home or stay on this page)
     window.location.href = '$CFG->wwwroot';
 </script>
 ";
