5a022c3a868875e50671c8b80b1ae2b9e469a893
EngineerUpSolutions <software.expertise.going@gmail.com>
2025-11-19 10:05:31 -0500

    version1.0 plugin

--- plugin/microcapsulas/db/access.php
+++ plugin/microcapsulas/db/access.php
@@ -1,41 +1,17 @@
 <?php
-// This file is part of Moodle - http://moodle.org/
-//
-// Moodle is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// Moodle is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
-
-/**
- * Plugin capabilities for the local_calificaciones plugin.
- *
- * @package   local_calificaciones
- * @copyright Equipo zajuna
- * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
- */
-// Definir las capacidades del plugin
 defined('MOODLE_INTERNAL') || die();
 
 $capabilities = [
 
-    // Ver calificaciones (solo lectura).
     'local/microcapsulas:view' => [
         'captype'      => 'read',
-        'contextlevel' => CONTEXT_COURSE,
+        'contextlevel' => CONTEXT_SYSTEM, // System-level, safest for launcher
         'archetypes'   => [
-            // 'manager'        => CAP_ALLOW,
-            // 'teacher'        => CAP_ALLOW,
-            // 'editingteacher' => CAP_ALLOW,
-            'student'        => CAP_ALLOW
+            'student'        => CAP_ALLOW,
+            'editingteacher' => CAP_ALLOW,
+            'teacher'        => CAP_ALLOW,
+            'manager'        => CAP_ALLOW
         ],
-    ]
+    ],
 
 ];
--- plugin/microcapsulas/version.php
+++ plugin/microcapsulas/version.php
@@ -5,26 +5,9 @@
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
-//
-// Moodle is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
-
-/**
- * Boost.
- *
- * @package    local_calificaciones
- * @author     Zajuna team 
- * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
- * @var stdClass $plugin
- */
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2025111800;     // Nueva versión, mayor a 2025101000
-$plugin->requires  = 2023100400;
+$plugin->version   = 2025111900; // Increase version number
+$plugin->requires  = 2023100400; // Moodle 4.3 minimum
 $plugin->component = 'local_microcapsulas';
