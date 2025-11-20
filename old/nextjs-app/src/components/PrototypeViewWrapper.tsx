b5f84c2d1a3da14441c027a04eeeb72dba5f9fe6
EngineerUpSolutions <software.expertise.going@gmail.com>
2025-11-20 09:54:27 -0500

    cleaning old front

--- old/nextjs-app/src/components/PrototypeViewWrapper.tsx
+++
@@ -1,6 +0,0 @@
-'use client'
-import dynamic from 'next/dynamic'
-const PrototypeView = dynamic(() => import('./PrototypeView.client'), { ssr: false })
-export default function PrototypeViewWrapper() {
-  return <PrototypeView />
-}
