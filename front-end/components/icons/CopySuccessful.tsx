4d0a2034849563adadf13cb2b2d4383647c5ecb3
EngineerUpSolutions <software.expertise.going@gmail.com>
2025-12-03 11:35:09 -0500

    working matching figma part7

---
+++ front-end/components/icons/CopySuccessful.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const CopySuccessful: React.FC<React.SVGProps<SVGSVGElement>> = (props) => {
+  return (
+      <svg width="28" height="28" viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <rect width="28" height="28" rx="7" fill="#00AA00"/>
+      <path d="M7.46582 15.8662L10.7325 19.1329L20.5325 8.86621" stroke="white" stroke-width="2.1" stroke-linecap="round" stroke-linejoin="round"/>
+      </svg>
+  );
+};
